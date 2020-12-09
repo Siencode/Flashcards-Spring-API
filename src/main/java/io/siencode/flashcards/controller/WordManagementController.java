700db47f09366f6dd34616a2f20119609162c474
Michał Sieńko <m.siencode@gmail.com>
2020-12-09 19:50:42 +0100

    controller: new features

--- src/main/java/io/siencode/flashcards/controller/WordManagementController.java
+++ src/main/java/io/siencode/flashcards/controller/WordManagementController.java
@@ -4,23 +4,23 @@ import io.siencode.flashcards.entity.Account;
 import io.siencode.flashcards.entity.Word;
 import io.siencode.flashcards.model.WordModel;
 import io.siencode.flashcards.repo.AccountRepository;
-import io.siencode.flashcards.repo.SelectedWordRepository;
 import io.siencode.flashcards.repo.WordRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class WordManagementController {
 
   private AccountRepository accountRepository;
-  private SelectedWordRepository selectedWordRepository;
   private WordRepository wordRepository;
 
-    public WordManagementController(AccountRepository accountRepository, SelectedWordRepository selectedWordRepository, WordRepository wordRepository) {
+    public WordManagementController(AccountRepository accountRepository, WordRepository wordRepository) {
         this.accountRepository = accountRepository;
-        this.selectedWordRepository = selectedWordRepository;
         this.wordRepository = wordRepository;
     }
 
@@ -40,6 +40,28 @@ public class WordManagementController {
         return wordModel;
     }
 
+    @PutMapping("/word/{id}")
+    public Word replaceWord(@RequestBody WordModel wordModel, @PathVariable Long id) {
+        return wordRepository.findById(id).map(word -> {
+            word.setWord_PL(wordModel.getWord_PL());
+            word.setWord_ENG(wordModel.getWord_ENG());
+            return wordRepository.save(word);
+        }).orElseGet(() -> {
+           Word word = new Word();
+           word.setWord_ENG(wordModel.getWord_ENG());
+           word.setWord_PL(wordModel.getWord_PL());
+           wordRepository.save(word);
+           return word;
+        });
+    }
 
-
+    @DeleteMapping("/word/{id}")
+    public void removeWord(@PathVariable Long id){
+        Optional<Word> optionalWord = wordRepository.findById(id);
+        if (optionalWord.isPresent()) {
+            wordRepository.deleteById(id);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
 }
