037efc92e6b12ccd9b3ded80c00220a577c728d1
Michał Sieńko <m.siencode@gmail.com>
2020-12-19 22:47:22 +0100

    renamed and updated userService

--- src/main/java/io/siencode/flashcards/controller/UserController.java
+++ src/main/java/io/siencode/flashcards/controller/UserController.java
@@ -1,38 +1,29 @@
 package io.siencode.flashcards.controller;
 
-import io.siencode.flashcards.entity.User;
 import io.siencode.flashcards.model.UserModel;
-import io.siencode.flashcards.repo.UserRepository;
+import io.siencode.flashcards.service.UserService;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api")
 public class UserController {
 
-    private final PasswordEncoder passwordEncoder;
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(PasswordEncoder passwordEncoder, UserRepository userRepository) {
-        this.passwordEncoder = passwordEncoder;
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/register")
     public void userRegister(@RequestBody UserModel userModel) {
-        Optional<User> userOptional = userRepository.findByUsername(userModel.getUsername());
-        if (userOptional.isPresent()) {
+        if (userService.userIsExist(userModel.getUsername())) {
             throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "The username exists. Can't be duplicated.");
         } else {
-            User user = new User();
-            user.setUsername(userModel.getUsername());
-            user.setPassword(passwordEncoder.encode(userModel.getPassword()));
-            userRepository.save(user);
+            userService.saveUser(userModel);
         }
     }
 
